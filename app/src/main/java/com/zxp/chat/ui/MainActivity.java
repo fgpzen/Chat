5c40382f91a80fc44ade43d5503c0a257368ec6e
zhuxiaopeng <zhu.xiaopeng@intellif.com>
2019-07-30 19:00:31 +0800

    接入环信SDK

--- app/src/main/java/com/zxp/chat/ui/MainActivity.java
+++ app/src/main/java/com/zxp/chat/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.zxp.chat;
+package com.zxp.chat.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.zxp.chat.R;
 
 public class MainActivity extends AppCompatActivity {
 
